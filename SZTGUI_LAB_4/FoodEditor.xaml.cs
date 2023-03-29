729a0a8511fe9765eef52a96aa45a938615de396
Yeti2000 <baba.banjamin@gmail.com>
2023-03-29 09:57:34 +0200

    FoodEditor, MainWindow adjustments

---
+++ SZTGUI_LAB_4/FoodEditor.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SZTGUI_LAB_4
+{
+    /// <summary>
+    /// Interaction logic for FoodEditor.xaml
+    /// </summary>
+    public partial class FoodEditor : Window
+    {
+        private Food food;
+        public Foods selectedType { get; set; }
+        private List<Foods> foods;
+        public FoodEditor(Food food)
+        {
+            this.food = food;
+            InitializeComponent();
+            this.DataContext = food;
+            foods = new List<Foods>()
+            {
+                Foods.Appetizer,
+                Foods.Drink,
+                Foods.MainCourse,
+                Foods.Dessert
+            };
+            typeBox.ItemsSource = foods;
+            typeBox.SelectedIndex = (int)food.Type;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            food.Name = nameBox.Text;
+            food.Type = typeBox.SelectedIndex >= 0 ? foods[typeBox.SelectedIndex] : food.Type;
+            int price = 0;
+            food.Price = int.TryParse(priceBox.Text, out price) ? price : food.Price;
+            this.DialogResult = true;
+        }
+    }
+}
--- SZTGUI_LAB_4/MainWindow.xaml.cs
+++ SZTGUI_LAB_4/MainWindow.xaml.cs
@@ -23,6 +23,26 @@ namespace SZTGUI_LAB_4
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            (DataContext as ViewModel).IsFilterOn = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            (DataContext as ViewModel).IsFilterOn = true;
+        }
+
+        private void menuBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = ItemsControl.ContainerFromElement(sender as ListBox, e.OriginalSource as DependencyObject) as ListBoxItem;
+            if (item != null)
+            {
+                new FoodEditor(item.DataContext as Food).ShowDialog();
+            }
         }
     }
 }
