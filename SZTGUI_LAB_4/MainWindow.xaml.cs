c4d34b4e3965b10dd0adeccec3cd89a8aedbbb1e
Yeti2000 <baba.banjamin@gmail.com>
2023-03-29 10:31:48 +0200

    Adding additional crap

--- SZTGUI_LAB_4/MainWindow.xaml.cs
+++ SZTGUI_LAB_4/MainWindow.xaml.cs
@@ -26,16 +26,6 @@ namespace SZTGUI_LAB_4
             this.DataContext = new ViewModel();
         }
 
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            (DataContext as ViewModel).IsFilterOn = false;
-        }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            (DataContext as ViewModel).IsFilterOn = true;
-        }
-
         private void menuBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             var item = ItemsControl.ContainerFromElement(sender as ListBox, e.OriginalSource as DependencyObject) as ListBoxItem;
@@ -43,6 +33,46 @@ namespace SZTGUI_LAB_4
             {
                 new FoodEditor(item.DataContext as Food).ShowDialog();
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if((DataContext as ViewModel).SelectedMenu != null)
+                (DataContext as ViewModel).OrderList.Add((DataContext as ViewModel).SelectedMenu);
+            if ((DataContext as ViewModel).SelectedOrder != null)
+            {
+                int sum = 0;
+                foreach (var item in (DataContext as ViewModel).OrderList)
+                    sum += item.Price;
+                sumLabel.Content = $"{(double)sum / (DataContext as ViewModel).OrderList.Count()} Ft";
+            }
+            sumLabel.Content = $"??? Ft";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if ((DataContext as ViewModel).SelectedOrder != null)
+                (DataContext as ViewModel).OrderList.Remove((DataContext as ViewModel).SelectedOrder);
+            if ((DataContext as ViewModel).SelectedOrder != null)
+            {
+                int sum = 0;
+                foreach (var item in (DataContext as ViewModel).OrderList)
+                    sum += item.Price;
+                sumLabel.Content = $"{(double)sum / (DataContext as ViewModel).OrderList.Count()} Ft";
+            }
+            sumLabel.Content = $"??? Ft";
+        }
+
+        private void ListBox_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            if ((DataContext as ViewModel).SelectedOrder != null)
+            {
+                int sum = 0;
+                foreach (var item in (DataContext as ViewModel).OrderList)
+                    sum += item.Price;
+                sumLabel.Content = $"{(double)sum / (DataContext as ViewModel).OrderList.Count()} Ft";
+            }
+            sumLabel.Content = $"??? Ft";
         }
     }
 }
