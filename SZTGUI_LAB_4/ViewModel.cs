11003e1a1acc35f13b2dc0ad7fad6bb0dd76b545
Tolnai Gergő <gergotolnai001@gmail.com>
2023-03-29 08:54:57 +0200

    ViewModel basics

--- SZTGUI_LAB_4/ViewModel.cs
+++ SZTGUI_LAB_4/ViewModel.cs
@@ -3,24 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace SZTGUI_LAB_4
 {
     public class ViewModel
     {
         private List<Food> menu;
+        private List<Foods> filters;
+        public bool isFilterOn;
+        public Foods selectedFilter;
+        public List<Food> Menu 
+        { 
+            get 
+            {
+                if(isFilterOn) 
+                {
+                    return GetFilteredMenu(selectedFilter);
+                }
+                else
+                {
+                    return menu;
+                }
+            } 
+        }
+        public List<Foods> Filters
+        {
+            get { return filters;}
+        }
         public ViewModel()
         {
             menu = new List<Food>()
             {
-                new Food("Sör", Food.Foods.Drink, 890),
-                new Food("Kóla", Food.Foods.Drink, 690),
-                new Food("Víz", Food.Foods.Drink, 450),
-                new Food("Lángos", Food.Foods.MainCourse, 1100),
-                new Food("Bolognai", Food.Foods.MainCourse, 1250),
-                new Food("Marlenka", Food.Foods.Dessert, 990),
-                new Food("Burger", Food.Foods.Appetizer, 890)
+                new Food("Sör", Foods.Drink, 890),
+                new Food("Kóla", Foods.Drink, 690),
+                new Food("Víz", Foods.Drink, 450),
+                new Food("Lángos", Foods.MainCourse, 1100),
+                new Food("Bolognai", Foods.MainCourse, 1250),
+                new Food("Marlenka", Foods.Dessert, 990),
+                new Food("Burger", Foods.Appetizer, 890)
             };
+
+        }
+        public List<Food> GetFilteredMenu(Foods filter) 
+        {
+            return menu.FindAll(x => x.Type == filter);
         }
 
     }
@@ -51,7 +78,7 @@ namespace SZTGUI_LAB_4
         {
             get { return price; }
             set { price = value; }
-
+        }
 
 
 
