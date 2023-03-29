7643c0360261e24dd6d7ec7ca0a0cfa58cfc6ac8
Tolnai Gergő <gergotolnai001@gmail.com>
2023-03-29 08:30:13 +0200

    VIewModel basics + Foods enum

---
+++ SZTGUI_LAB_4/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTGUI_LAB_4
+{
+    public class ViewModel
+    {
+        
+    }
+    public class Food
+    {
+
+    }
+    public enum Foods
+    {
+        Appetizer, Drink, MainCourse, Dessert
+    }
+}
